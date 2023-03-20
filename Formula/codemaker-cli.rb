da40e56864bcff16320e6eb2f2532b72d2799679
CodeMaker Dev <codemaker-dev@codemaker.ai>
2023-03-19 22:53:29 -0700

    Fixed lint errors

--- Formula/codemaker-cli.rb
+++ Formula/codemaker-cli.rb
@@ -1,16 +1,16 @@
 class CodemakerCli < Formula
-    desc "CodeMaker CLI."
-    homepage "https://github.com/codemakerai/codemaker-cli"
-    url "https://github.com/codemakerai/codemaker-cli.git",
-          tag:      "v0.0.1",
-          revision: "adf6713a4e1e01e47a337f410e22cf3ffb74eb5f"
-    head "https://github.com/codemakerai/codemaker-cli.git", branch: "dev"
-    license "MIT"
+  desc "Installs CodeMaker CLI"
+  homepage "https://github.com/codemakerai/codemaker-cli"
+  url "https://github.com/codemakerai/codemaker-cli.git",
+      tag:      "v0.0.1",
+      revision: "adf6713a4e1e01e47a337f410e22cf3ffb74eb5f"
+  license "MIT"
+  head "https://github.com/codemakerai/codemaker-cli.git", branch: "dev"
 
-    depends_on "go" => :build
-  
-    def install
-      system "go", "build", *std_go_args(ldflags: "-s -w")
-      bin.install_symlink "codemaker-cli" => "codemaker"
-    end
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+    bin.install_symlink "codemaker-cli" => "codemaker"
   end
+end
