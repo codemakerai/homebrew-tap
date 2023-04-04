c8cc68e54df9fc60efc3c05f06024c9c4898fbd9
CodeMaker Dev <codemaker-dev@codemaker.ai>
2023-04-04 01:33:55 -0700

    Release v0.0.4

--- Formula/codemaker-cli.rb
+++ Formula/codemaker-cli.rb
@@ -2,8 +2,8 @@ class CodemakerCli < Formula
   desc "Installs CodeMaker CLI"
   homepage "https://github.com/codemakerai/codemaker-cli"
   url "https://github.com/codemakerai/codemaker-cli.git",
-      tag:      "v0.0.3",
-      revision: "ac0eda1378ddebf4547436ee3dc1c5abfefeb67a"
+      tag:      "v0.0.4",
+      revision: "b1f863d35961e5db45be9b513d161ea707bd1de3"
   license "MIT"
   head "https://github.com/codemakerai/codemaker-cli.git", branch: "dev"
 
